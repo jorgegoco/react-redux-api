fb06b3d87e0ffc8f06f3bc22f34a14ad208a5947
jorgegoco <jorgegoco70@gmail.com>
2023-01-17 20:44:26 +0100

    Set up Redux store

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,11 @@
+import ActionTypes from '../constants/action-types';
+
+export const setProducts = (products) => ({
+  type: ActionTypes.SET_PRODUCTS,
+  payload: products,
+});
+
+export const selectedProduct = (product) => ({
+  type: ActionTypes.SELECTED_PRODUCT,
+  payload: product,
+});
